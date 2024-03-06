dfddcd7c78fe9262f77b5693c95f834d60ff169d
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-06 23:40:59 +0530

    fix: props color

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,7 @@ export function Ratings(props) {
     <div className='w-full flex flex-col items-center justify-center h-full'>
         <div className='md:w-[100px] px-8 w-full justify-between bg-blue-100/60 bg rounded-b-2xl h-[100px] flex md:flex-col md:justify-center items-center'>
             <h1 className='text-blue-500 font-normal text-[32px]'>{props.rating}</h1>
-            <h1 className='text-[18px] md:text-[12px] text-nowrap '>{props.remarks}</h1>
+            <h1 className='text-[18px] text-blue-600 md:text-[12px] text-nowrap '>{props.remarks}</h1>
             <span><Rating size='small' name="half-rating" defaultValue={2.5} value={props.stars} precision={0.5} /></span>
         </div>
         <div className='flex h-full items-end md:mt-0 mt-8'>
